db2e7680e9d81adc6c7db22403c377b2c243bbd6
Jonathan Longobardi <64801320+JonathanLongoardi@users.noreply.github.com>
2021-12-14 16:46:40 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT
+const PORT = process.env.PORT || 80;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
